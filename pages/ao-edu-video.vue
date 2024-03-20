3672391337547647662285efec509e4dfdf382ec
HelloRWA <dev@rwa-nft.com>
2024-03-21 05:51:06 +0800

    ✨ add link

---
+++ pages/ao-edu-video.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+navigateTo('https://t.co/J3Ko1znTkh', { external: true })
+</script>
+<template>
+  <div>
+    Loading...
+  </div>
+</template>
---
+++ pages/ao-x-thread.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+navigateTo('https://twitter.com/StarkEVM99/status/1770567325416350166', { external: true })
+</script>
+<template>
+  <div>
+    Loading...
+  </div>
+</template>
